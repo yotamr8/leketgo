155d100cd740a3689cc70d799f6bb1a7bfd3c9ef
Bar Amir <baramir93@gmail.com>
2019-05-07 23:15:05 +0300

    NextJS

---
+++ pages/task-reports.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../statics/styles.scss"
+import Header from '../components/Header.js'
+
+export default function Index() {
+  return (
+    <div>
+      <Header />
+      <main></main>
+    </div>
+  )
+}
